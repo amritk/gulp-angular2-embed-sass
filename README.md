55ed06d9cb6b1c690077c17f12fdccb609be3556
Amrit Kahlon <amrit@hockey-community.com>
2015-12-29 18:19:25 -0800

    it works!

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # gulp-angular2-embed-sass
 A gulp plugin to compile styleUrls with sass to css and include them as strings under style.
 
+Needs massive cleanup, refactorization, options but it works!
+
 Based on https://github.com/laxa1986/gulp-angular-embed-templates
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var through     = require('through2');
-var gutil       = require('gulp-util');
-var pathModule  = require('path');
-var sass        = require('node-sass');
-var fs          = require('fs');
-var PluginError = gutil.PluginError;
-// var jsStringEscape = require('js-string-escape');
+var through        = require('through2');
+var gutil          = require('gulp-util');
+var pathModule     = require('path');
+var sass           = require('node-sass');
+var fs             = require('fs');
+var jsStringEscape = require('js-string-escape');
+var PluginError    = gutil.PluginError;
+
 
 // Constants
 const PLUGIN_NAME = 'gulp-angular2-embed-sass';
@@ -14,15 +15,22 @@ module.exports = function (options) {
     options = options || {};
 
     var content;
-    var styleUrlRegexp =  /[\'"\s]*styleUrls[\'"\s]*:[\s]*\[(.*)\]/;
+    var styleUrlRegexp =  /styleUrls[\'"\s]*:[\s]*\[(.*)\]/;
 
     const FOUND_SUCCESS = {};
     const FOUND_ERROR = {};
     const FOUND_IGNORE = {};
     const CODE_EXIT = {};
 
-    const TEMPLATE_BEGIN = Buffer('style:\'');
-    const TEMPLATE_END = Buffer('\'');
+    const TEMPLATE_BEGIN = Buffer('styles: [\'');
+    const TEMPLATE_END = Buffer('\']');
+
+    var debug = true;
+    function log() {
+        if (debug) {
+            console.log.apply(console, arguments);
+        }
+    }
 
     /**
      * Find next "styleUrls:", and try to replace url with content if template available, less then maximum size.
@@ -31,83 +39,42 @@ module.exports = function (options) {
      * @param {String} filePath path to original .js file
      * @param {Function} cb callback function to call when
      */
-    function replace(filePath, cb) {
+    function replace(filePath, cb, relativeSassPath, matches, done) {
 
-        var matches = styleUrlRegexp.exec(content);
+        var path = pathModule.join(filePath, relativeSassPath);
 
-        if (matches === null) {
-            cb(CODE_EXIT);
-            return;
-        }
+        sass.render({
+          file: path,
+        }, function(err, result) {
 
-        var relativeTemplatePath = matches[1].replace(/['"]/g,"").split(',');
-        var path = pathModule.join(filePath, relativeTemplatePath[0]);
-        console.log(filePath);
-        console.log(relativeTemplatePath[0]);
-        console.log(path);
+            cb(FOUND_SUCCESS, {
+                regexpMatch : matches,
+                template: result.css.toString()
+            });
 
-        console.log('sass path: ' + path);
-
-        if (options.maxSize) {
-            var fileStats = fs.statSync(path);
-            if (fileStats && fileStats.size > options.maxSize) {
-                return cb(FOUND_IGNORE, {
-                    path: relativeTemplatePath,
-                    size: fileStats.size
-                });
-            }
-        }
-
-        fs.readFile(path, {encoding: 'utf-8'}, function(err, templateContent) {
-
-            console.log(templateContent);
-            console.log(err);
-            if (err) {
-                console.log('eee');
-                cb(FOUND_ERROR, 'Can\'t read template file: "' + path + '". Error details: ' + err);
-                return;
-            }
-
-            console.log(templateContent);
-
-            // cb(FOUND_SUCCESS, {
-            //     regexpMatch : matches,
-            //     template: templateContent
-            // });
-
-            // sass.render({
-            //   file: scss_filename,
-            //   [, options..]
-            // }, function(err, result) { /*...*/ });
-
-            // minimizer.parse(templateContent, function (err, minifiedContent) {
-            //     if (err) {
-            //         cb(FOUND_ERROR, 'Error while minifying angular template "' + path + '". Error from "minimize" plugin: ' + err);
-            //         return;
-            //     }
-
-            //     cb(FOUND_SUCCESS, {
-            //         regexpMatch : matches,
-            //         template: minifiedContent
-            //     });
-            // });
+            if(done) cb(CODE_EXIT);
         });
     }
 
     function joinParts(entrances) {
         var parts = [];
         var index = 0;
+        var matches = entrances[0].regexpMatch;
+
+        parts.push(Buffer(content.substring(index, matches.index)));
+        parts.push(TEMPLATE_BEGIN);
+
         for (var i=0; i<entrances.length; i++) {
-            var entrance = entrances[i];
-            var matches = entrance.regexpMatch;
 
-            parts.push(Buffer(content.substring(index, matches.index)));
-            parts.push(TEMPLATE_BEGIN);
-            parts.push(Buffer(jsStringEscape(entrance.template)));
-            parts.push(TEMPLATE_END);
+            parts.push(Buffer(entrances[i].template.replace(/\n/g, '')));
 
-            index = matches.index + matches[0].length;
+            if (i < entrances.length - 1) {
+                parts.push(Buffer('\', \''));
+            }
+
         }
+        parts.push(TEMPLATE_END);
+        index = matches.index + matches[0].length;
         parts.push(Buffer(content.substr(index)));
         return Buffer.concat(parts);
     }
@@ -128,15 +95,30 @@ module.exports = function (options) {
         content = file.contents.toString('utf-8');
         var entrances = [];
 
-        console.log('\nfile.path: ' + file.path);
+        log('\nfile.path: ' + file.path);
 
         var base = options.basePath ? options.basePath : pathModule.dirname(file.path);
-        replace(base, replaceCallback);
+        var matches = styleUrlRegexp.exec(content);
+
+        log('matches: ' + matches[1]);
+
+        if (matches === null) {
+            replaceCallback(CODE_EXIT);
+            return;
+        }
+
+        var relativeTemplatePaths = matches[1].replace(/[\ '"]/g,"").split(',');
+        var entrances = [];
+
+        for (var i = 0; i < relativeTemplatePaths.length; i++) {
+            replace(base, replaceCallback, relativeTemplatePaths[i], matches, i === relativeTemplatePaths.length - 1);
+        }
+
 
         function replaceCallback(code, data) {
+
             if (code === FOUND_SUCCESS) {
                 entrances.push(data);
-                replace(base, replaceCallback);
             }
 
             else if (code === FOUND_ERROR) {
