8bfa1be09568407aa6a9a32bd946eb460d96383e
Fernando Gomez <fernando.gomez@Golden-Race.local>
2016-10-17 08:36:13 +0100

    Less compiler. \n Add type option. \n Test from file with fix windows compatibility. \n Nested rules test.

--- test/mocha.js
+++ test/mocha.js
@@ -1,6 +1,6 @@
 require('mocha');
 
-var embedSass = require('../');
+var embedSass = require('../index.js');
 var assert    = require('assert');
 var fs        = require('fs');
 var File      = require('gulp-util').File;
@@ -16,6 +16,8 @@ describe('gulp-angular2-embed-sass', function () {
         var fakeFile = new File({
             contents: new Buffer(fs.readFileSync('test/assets/test.component.ts'))
         });
+        
+        fakeFile.path = 'test.component.ts';
         emb.write(fakeFile);
         emb.once('data', function (file) {
 
@@ -24,13 +26,34 @@ describe('gulp-angular2-embed-sass', function () {
             // check the contents
             //
             assert.equal(file.contents.toString('utf8'),
-                'import {Component} from \'angular2/core\';\n\n' +
-                '@Component({\n' +
-                '    selector: \'app\',\n' +
-                '    templateUrl: \'test/assets/test.component.html\',\n' +
-                '    styles: [`h1 {  font-size: 200px; }\', \'h5 {  color: red; }`]\n' +
-                '})\n\n' +
-                'export class AppComponent { }\n'
+                        new Buffer(fs.readFileSync('test/assets/assert.component.ts')).toString('utf8')
+            );
+            done();
+        });
+    });
+});
+
+describe('gulp-angular2-embed-sass', function () {
+    it('less: should embed style content whenever specified styleUrls', function (done) {
+
+        // Create a 'gulp-angular2-embed-sass' plugin stream
+        //
+        var emb = embedSass({type: 'less'});
+
+        var fakeFile = new File({
+            contents: new Buffer(fs.readFileSync('test/assets/testless.component.ts'))
+        });
+        
+        fakeFile.path = 'testless.component.ts';
+        emb.write(fakeFile);
+        emb.once('data', function (file) {
+
+            assert(file.isBuffer());
+
+            // check the contents
+            //
+            assert.equal(file.contents.toString('utf8'),
+                        new Buffer(fs.readFileSync('test/assets/assertless.component.ts')).toString('utf8')
             );
             done();
         });
