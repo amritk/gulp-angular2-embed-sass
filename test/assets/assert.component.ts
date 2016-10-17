b8f50607c198368b0307a07d42f31f748921b243
Fernando Gomez <fernando.gomez@Golden-Race.local>
2016-10-17 08:34:49 +0100

    Less compiler. \n Add type option. \n Test from file with fix windows compatibility. \n Nested rules test.

---
+++ test/assets/assert.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: 'test/assets/test.component.html',
+    styles: [`h1 {  font-size: 200px; }', 'h5 {  color: red; }  h5 .title {    color: blue; }`]
+})
+
+export class AppComponent { }
---
+++ test/assets/testless.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: 'test/assets/test.component.html',
+    styleUrls: ['test/assets/testless.component.less', 'test/assets/testless.component2.less']
+})
+
+export class AppComponent { }
