9f40f183a6e47ab2cb0721efd1432f239aad8380
Szabó Richárd <richard.szabo@erinnovations.net>
2017-03-03 19:40:47 +0100

    Update tests: use backticks constistently in 1-2. test assets.

--- test/assets/assert.component.ts
+++ test/assets/assert.component.ts
@@ -3,7 +3,7 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'app',
     templateUrl: 'test/assets/test.component.html',
-    styles: [`h1 {  font-size: 200px; }', 'h5 {  color: red; }  h5 .title {    color: blue; }`]
+    styles: [`h1 {  font-size: 200px; }`, `h5 {  color: red; }  h5 .title {    color: blue; }`]
 })
 
 export class AppComponent { }
